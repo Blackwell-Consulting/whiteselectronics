7671c861558ac1363aa12019405d50dbb17b5e7e
Mark Busnelli Jr <encryptdesigns@gmail.com>
2017-08-30 11:39:20 -0600

    query updated

--- wp-content/themes/whites-electronics/public/assets/js/custom.js
+++ wp-content/themes/whites-electronics/public/assets/js/custom.js
@@ -3,91 +3,89 @@ Custom
 ******/
 whitesElectronics = {
 
-    init: function() {
+        init: function() {
 
-        whitesElectronics.setupLocaleLightbox();
+            whitesElectronics.setupLocaleLightbox();
 
-        $( window ).on( 'resize', whitesElectronics.debounce( function( ) {}, 500 ) );
+            $( window ).on( 'resize', whitesElectronics.debounce( function( ) {}, 500 ) );
 
-        $( window ).load( function() {
-        } );
-    },
+            $( window ).load( function() {
+            } );
+        },
 
-    setupLocale: function() {
-        var vars = [], hash;
-        var hashes = window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ).split( '&' );
-        for( var i = 0; i < hashes.length; i++ ) {
-            hash = hashes[i].split( '=' );
-            vars.push( hash[0] );
-            vars[ hash[0] ] = hash[1];
-        }
-        return vars;
-    },
+        setupLocale: function() {
+            var vars = [], hash;
+            var hashes = window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ).split( '&' );
+            for( var i = 0; i < hashes.length; i++ ) {
+                hash = hashes[i].split( '=' );
+                vars.push( hash[0] );
+                vars[ hash[0] ] = hash[1];
+            }
+            return vars;
+        },
 
-    setupLocaleLightbox: function() {
-        var lang = whitesElectronics.setupLocale()[ 'lang' ];
+        setupLocaleLightbox: function() {
+            var lang = whitesElectronics.setupLocale()[ 'lang' ];
 
-        if ( typeof lang !== 'undefined' ) {
-            console.log('------------------------------------');
-            console.log(lang);
-            console.log('------------------------------------');
-        } else {
-            $( '.lightbox' ).delay( '200ms' ).fadeIn();
-        }
-    },
-
-    appendQueryStringToUrls: function() {
-
-        $( 'a[href]' ).each( function() {
-            var $this = $( this );
-            var href = $this.attr( 'href' );
-
-            if ( href.indexOf( '?' ) !== -1 ) {
-                // Query string exists, append current query string
-                href += '&' + location.search.replace( /^\?/, '' );
+            if ( typeof lang !== 'undefined' ) {
+                whitesElectronics.appendQueryStringToUrls();
             } else {
-                // No query string yet, add it
-                href += location.search;
+                $( '.lightbox' ).delay( '200ms' ).fadeIn();
             }
+        },
 
-            if ( ! $this.hasClass( 'language-picker' ) ) {
-                $this.attr( 'href', href );
-            }
-        } );
-    }
+        appendQueryStringToUrls: function() {
 
-} // end object
+            $( 'a[href]' ).each( function() {
+                var $this = $( this );
+                var href = $this.attr( 'href' );
 
-// Debounce function used to throttle frequency
-whitesElectronics.debounce = function( func, wait, immediate ) {
-    var timeout;
-    return function() {
-        var context = this,
-            args = arguments;
-        var later = function() {
-            timeout = null;
-            if ( ! immediate ) func.apply( context, args );
+                if ( href.indexOf( '?' ) !== -1 ) {
+                    // Query string exists, append current query string
+                    href += '&' + location.search.replace( /^\?/, '' );
+                } else {
+                    // No query string yet, add it
+                    href += location.search;
+                }
+
+                if ( ! $this.hasClass( 'language-picker' ) ) {
+                    $this.attr( 'href', href );
+                }
+            } );
+        }
+
+    } // end object
+
+    // Debounce function used to throttle frequency
+    whitesElectronics.debounce = function( func, wait, immediate ) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var later = function() {
+                timeout = null;
+                if ( ! immediate ) func.apply( context, args );
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout( timeout );
+            timeout = setTimeout( later, wait );
+            if ( callNow ) func.apply( context, args );
         };
-        var callNow = immediate && !timeout;
-        clearTimeout( timeout );
-        timeout = setTimeout( later, wait );
-        if ( callNow ) func.apply( context, args );
     };
-};
 
-whitesElectronics.isMobile = function() {
-    return $( window ).width() < 960 ? true : false;
-};
+    whitesElectronics.isMobile = function() {
+        return $( window ).width() < 960 ? true : false;
+    };
 
-/**
- * Runs on window resize every 50ms
- * Calculates css line-height on the slide overlays
- *
- */
-whitesElectronics.resizeThrottle = whitesElectronics.debounce( function() {
-    whitesElectronics.productDetail();
-}, 50 );
+    /**
+     * Runs on window resize every 50ms
+     * Calculates css line-height on the slide overlays
+     *
+     */
+    whitesElectronics.resizeThrottle = whitesElectronics.debounce( function() {
+        whitesElectronics.productDetail();
+    }, 50 );
 
-$( document ).ready( function() {
-    whitesElectronics.init();
-} );
+    $( document ).ready( function() {
+        whitesElectronics.init();
+    } );
